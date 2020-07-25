61fffef30b58bcc24d1a59ff18c3e7c54a0970d5
A.H.M. (Mari) Donkers <a.h.m.donkers@photonsphere.org>
2020-07-25 11:02:27 +0200

    Added documentation to top of default.nix file.

--- xmonadconfig/default.nix
+++ xmonadconfig/default.nix
@@ -1,3 +1,15 @@
+# To initialize project use:
+#   nix-shell --packages ghc --run 'cabal init -p xmonadconfig'
+# To enter a Nix-shell use:
+#  nix-shell -A env
+# (leave and re-enter when errors after .cabal file changed)
+#
+# To build use:
+#  cabal v2-build
+#
+# In Emacs use C-l to start GHCi; or use the following from command line:
+#  cabal v2-repl
+#
 { pkgs ? import <nixpkgs> {} }:
 
 pkgs.haskellPackages.callCabal2nix "xmonadconfig" ./. {}
