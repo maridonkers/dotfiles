f4be8044febf2e0a8632490b3fcb151accd9ddda
A.H.M. (Mari) Donkers <a.h.m.donkers@photonsphere.org>
2021-11-15 13:02:25 +0100

    Revised comment.

--- xmonadconfig/shell.nix
+++ xmonadconfig/shell.nix
@@ -1,18 +1,6 @@
 # This file is NixOS specific.
 #
-# Required: cabal2nix
-# To initialize project --which has already been done here, so don't-- use the following:
-#   nix-shell --packages ghc --run 'cabal init -p xmonadconfig'
-#
-# To enter a Nix-shell use the following (from within ~/.xmonad subdirectory; i.e. symbolic link):
-#  nix-shell  (leave and re-enter when errors after .cabal file changed)
-#
-# To build use the following (from within ~/.xmonad):
-#  cabal v2-build
-#
-# In Emacs use C-l to start GHCi; or use the following from command line:
-#  cabal v2-repl
-#
+# Use make shell and subsequent make rebuild to check your source code.
 # In Emacs accept .dir-locals.el prompt if you use Dante (dante-mode) and get errors.
 #
 { pkgs ? import <nixpkgs> {} }:
