3ef6957e68c66f34846f851eda06548b6c9ff06c
A.H.M. (Mari) Donkers <a.h.m.donkers@photonsphere.org>
2025-02-15 13:11:57 +0100

    Move repository to Codeberg

--- bin/xrandr-hdmi-off.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-xrandr --output LVDS-1 --primary --auto --mode 1366x768 --pos 1920x312 --rotate normal --output VGA-1 --off --output HDMI-1 --off --output DP-1 --off
--- xmonadconfig/shell.nix
+++
@@ -1,8 +0,0 @@
-# This file is NixOS specific.
-#
-# Use make shell and subsequent make rebuild to check your source code.
-# In Emacs accept .dir-locals.el prompt if you use Dante (dante-mode) and get errors.
-#
-{ pkgs ? import <nixpkgs> {} }:
-
-(pkgs.haskellPackages.callCabal2nix "xmonadconfig" ./. {}).env
